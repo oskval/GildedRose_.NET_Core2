043455760c360233b2bf300cbc5eb091adfd567b
oskval <oskaras.valentinavicius@gmail.com>
2021-01-03 23:04:53 +0200

    Added Switch.cs for testing switch

---
+++ Switch.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace csharpcore
+{
+    public class Switch
+    {
+        IList<Item> Items;
+        public Switch(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+
+        public void UpdateQuality()
+        {
+            
+        }
+    }
+}
