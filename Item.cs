c24da2f629358bb5985da278d79beade087beade
oskval <oskaras.valentinavicius@gmail.com>
2021-01-04 18:49:31 +0200

    fixed up some mistakes

--- Item.cs
+++ Item.cs
@@ -2,14 +2,6 @@
 {
     public class Item
     {
-        public Item(){}
-        public Item(string name, int sellin, int quality)
-        {
-            Name = name;
-            SellIn = sellin;
-            Quality = quality;
-        }
-        
         public string Name { get; set; }
         public int SellIn { get; set; }
         public int Quality { get; set; }
